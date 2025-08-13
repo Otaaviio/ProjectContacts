51acee9c96e582cdbd20ea480ea652d7d022ae45
Otaaviio <inabaotavio7@gmail.com>
2025-08-13 16:43:37 -0300

    fix: correct import of contatoController in routes

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const route = express.Router();
 
 const homeController = require("./src/controllers/homeController");
 const loginController = require("./src/controllers/loginController");
-const contatoController = require("./src/controllers/loginController");
+const contatoController = require("./src/controllers/contatoController");
 
 const { loginRequired } = require("./src/middlewares/middleware");
 
