e36fa7ebd6319c2f56a58048e38b3e7686da6111
Otaaviio <inabaotavio7@gmail.com>
2025-08-13 14:00:11 -0300

    feat: initialize project with Express, MongoDB, and EJS
    - Added routes for home and login functionalities
    - Created server setup with session management and CSRF protection
    - Implemented home and login controllers
    - Developed middleware for global error handling and CSRF token management
    - Established Mongoose models for Home and Login
    - Created views for 404 error, login, and index pages with Bootstrap styling
    - Added message includes for success and error notifications
    - Configured Webpack for JavaScript bundling

---
+++ src/controllers/loginController.js
@@ -0,0 +1,28 @@
+const Login = require("../models/LoginModel");
+
+exports.index = (req, res) => {
+  if (req.session.user) return res.render("login-logado");
+  res.render("login");
+};
+
+exports.register = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+
+    if (login.errors.length > 0) {
+      req.flash("errors", login.errors);
+      req.session.save(() => {
+        return res.redirect("back");
+      });
+      return;
+    }
+    req.flash("success", "Usuário cadastrado com sucesso");
+    req.session.save(() => {
+      return res.redirect("back");
+    });
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
+};
